d317fa62ff9e1ec99895f5d91db5da430276cad0
Mirko van Ede <mvanede@bol.com>
2021-12-07 08:08:09 +0100

    Day 6

--- new_day.py
+++ new_day.py
@@ -12,8 +12,20 @@ except FileExistsError:
     exit()
 
 # Create files
-first_lines = """f = open("ass-{}-test-input.txt", "r")
-lines = f.read().split("\\n\\n")
+first_lines = """from utils import Parser
+from utils.lib import get_timer, panswer, pruntime
+_ST = get_timer()
+
+f = open("ass-{}-test-input.txt", "r")
+commands = Parser.split_by(f.read(), "\\n", conv_func=None)  # lambda x:int(x)
+
+# CODE HERE
+
+
+panswer("answer")
+pruntime(_ST)
+
+
 """.format(base_name)
 
 (p / ("ass-" + base_name + "-input.txt")).write_text('', encoding="utf-8")
--- utils/lib.py
+++ utils/lib.py
@@ -1,8 +1,17 @@
-import itertools
-
+import timeit
 
 depth_of_nested_lists = lambda L: isinstance(L, list) and max(map(depth_of_nested_lists, L)) + 1
 
-flatten_nested_lists = lambda x: [i for e in x for i in (flatten_nested_lists(e) if isinstance(e,list) else [e])]
+flatten_nested_lists = lambda x: [i for e in x for i in (flatten_nested_lists(e) if isinstance(e, list) else [e])]
 
 max_in_list = lambda lst: max(flatten_nested_lists(lst))
+
+
+def get_timer():
+    return timeit.default_timer()
+
+def pruntime(start_timer):
+    print('Finished in {:.5f}ms'.format(timeit.default_timer() - start_timer))
+
+def panswer(a):
+    print("ANSWER: {}".format(str(a)))
